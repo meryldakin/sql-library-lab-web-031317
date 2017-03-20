a78377b38f816a8466651b9c7995810276ca3f4b
meryldakin <meryl.k.dakin@gmail.com>
2017-03-20 19:37:54 -0400

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,17 +1,49 @@
 def select_books_titles_and_years_in_first_series_order_by_year
+  "SELECT books.title, books.year
+  FROM books
+  INNER JOIN series
+  ON books.series_id = series.id
+  WHERE series.id = 1
+  ORDER BY books.year;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
+  "SELECT characters.name, characters.motto
+  FROM characters
+  ORDER BY LENGTH(characters.motto) DESC LIMIT 1;"
 end
 
 def select_value_and_count_of_most_prolific_species
+  "SELECT species, COUNT(species)
+  FROM characters
+  GROUP BY species
+  ORDER BY COUNT(species) DESC LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
+  "SELECT authors.name, subgenres.name
+  FROM series
+  INNER JOIN authors
+    ON series.author_id = authors.id
+  INNER JOIN subgenres
+    ON subgenres.id = series.subgenre_id;"
 end
 
 def select_series_title_with_most_human_characters
+  "SELECT series.title
+  FROM series
+  INNER JOIN characters
+    ON characters.series_id = series.id
+  GROUP BY series.title
+  ORDER BY SUM(CASE WHEN characters.species = 'human' THEN 1 ELSE 0 END) DESC LIMIT 1;"
+
+
 end
 
 def select_character_names_and_number_of_books_they_are_in
+  "SELECT characters.name, SUM(character_books.character_id = characters.id) AS characters_books
+  FROM characters, character_books
+  GROUP BY characters.name
+  ORDER BY characters_books DESC;"
+
 end
